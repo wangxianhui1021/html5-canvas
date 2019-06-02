ade66ed78b6d79df5ec0fea653a50c3f1e30730d
Xianhui Wang <lizzy@192.168.0.103>
2019-06-02 14:25:53 +0800

    created basic html page with canvas and textbox overlay effect

---
+++ canvas.js
@@ -0,0 +1,149 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// c.fillRect(100,100,100,100);
+// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+// c.fillRect(400,100,100,100);
+// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+// c.fillRect(300,100,100,100);
+// c.fillRect(300,500,100,100);
+
+// //line
+
+// c.beginPath();
+// c.moveTo(50,300);
+// c.lineTo(300,100);
+// c.lineTo(400,300);
+// c.strokeStyle = '#fa34a3';
+// c.stroke();
+
+// //Arc
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle='blue';
+// c.stroke();
+
+var mouse={
+	x:undefined,
+	y:undefined
+};
+
+var maxRadius=50;
+
+
+var colorArray=[
+      '#5185AC',
+      '#9C6AAE',
+      '#BBC254',
+      '#4D2F08',
+      '#C3A173',
+      '#451656'
+
+
+];
+
+window.addEventListener('mousemove', 
+	function(event){
+		mouse.x=event.x;
+	    mouse.y=event.y;
+      
+});
+
+window.addEventListener('resize', function(){
+	canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init();
+})
+
+function Circle(x,y,dx,dy,radius){
+	this.x=x;
+	this.y=y;
+	this.dx=dx;
+	this.dy=dy;
+	this.radius = radius;
+	this.minRadius=radius;
+	this.color = colorArray[Math.floor(Math.random()*colorArray.length)];
+	
+	this.draw=function(){
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.strokeStyle='blue';
+      c.fillStyle = this.color;
+      c.fill();
+	};
+
+	this.update=function(){
+		if (this.x+this.radius>innerWidth|| this.x-this.radius<0){
+	             this.dx=-this.dx;
+             };
+
+        if (this.y+this.radius>innerHeight|| this.y-this.radius<0){
+	             this.dy=-this.dy;
+             };
+                
+		         this.x+=this.dx;
+
+		         this.y+=this.dy;
+
+         //interactivity
+         if (mouse.x-this.x<50 &&mouse.x-this.x>-50&&mouse.y-this.y<50 &&mouse.y-this.y>-50){
+         	
+         	if(this.radius<maxRadius){
+
+         		this.radius =this.radius + 1;
+         	}
+         	
+         }
+         else if(this.radius>this.minRadius){
+
+         	this.radius -=1;
+         	
+         }
+
+         this.draw();
+     };
+     
+	
+}
+
+       var circleArray=[];
+
+
+			function init(){
+				
+				circleArray=[];
+             
+				for( var i=0; i<900; i++){
+					var x=Math.random() * window.innerWidth;
+					var y=Math.random() * window.innerHeight;
+					var dx=Math.random()-0.5;
+					var dy=Math.random()-0.5;
+					var radius=Math.random()*8+1;
+
+					circleArray.push(new Circle(x,y,dx,dy,radius));
+					
+				}
+
+			}
+
+			function animate(){
+				
+
+				requestAnimationFrame(animate);
+				c.clearRect(0,0,innerWidth, innerHeight);
+			    
+			     for(var i =0; i<circleArray.length; i++){
+
+			     	  circleArray[i].update();
+			     }
+			 }
+			   
+
+            init();
+			animate();
